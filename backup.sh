9fc84c04ca07975b99aba80273f2ef571718334c
carlstronaut <carlnys@kth.se>
2020-03-17 14:59:05 +0000

    auto-commit from carlnys@nystromer-vm on Tue Mar 17 14:59:04 UTC 2020

---
+++ backup.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+cp /home/carlnys/*.ipynb /home/carlnys/vm-backups
+message="auto-commit from $USER@$(hostname -s) on $(date)"
+GIT=`which git`
+REPO_DIR=/home/carlnys/vm-backups
+cd ${REPO_DIR}
+${GIT} add --all .
+${GIT} commit -m "$message"
+${GIT} push git@github.com:carlstronaut/vm-backups.git master 
