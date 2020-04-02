9e5a44b3d7ab92bcc1f0e5372fcc89011c52f1e7
carlstronaut <carlnys@kth.se>
2020-04-02 13:16:38 +0000

    auto-commit from carlnys@nystromer-vm on Thu Apr  2 13:16:37 UTC 2020

--- backup.sh
+++ backup.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 cp /home/carlnys/*.ipynb /home/carlnys/vm-backups
-cp /home/carlnys/*.pt /home/carlnys/vm-backups
+cp /home/carlnys/*.pt /home/carlnys/vm-backups/saved_models
 message="auto-commit from $USER@$(hostname -s) on $(date)"
 GIT=`which git`
 REPO_DIR=/home/carlnys/vm-backups
